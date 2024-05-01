54913d788dca233f5fa167f18137d99ea875020d
AbdullahVanlioglu <a.vanli2019@gmail.com>
2024-05-01 18:30:02 +0300

    konoha sempo

--- transformer/llama_transformer.py
+++ transformer/llama_transformer.py
@@ -16,7 +16,7 @@ class ModelArgs():
     vocab_size: int = -1 # This will be set when we load the tokenizer
     multiple_of: int = 256
     ffn_dim_multiplayer: Optional[int] = None
-    norm_ep: float = 1e-5
+    norm_eps: float = 1e-5
 
     # Needed for KV cache
     max_batch_size: int = 32
@@ -24,12 +24,76 @@ class ModelArgs():
 
     device: str = None
 
+def precompute_theta_pos_frequencies(head_dim: int, seq_len: int, device: str, theta: float = 10000.0):
+    # As written in the paragraph 3.2.2 of the paper
+    # >> In order to generalize our results in 2D to any xi ∈ Rd where **d is even**, [...]
+    assert head_dim % 2 == 0, "Dimension must be divisible by 2"
+    # Build the theta parameter
+    # According to the formula theta_i = 10000^(-2(i-1)/dim) for i = [1, 2, ... dim/2]
+    # Shape: (Head_Dim / 2)
+    theta_numerator = torch.arange(0, head_dim, 2).float()
+    # Shape: (Head_Dim / 2)
+    theta = 1.0 / (theta ** (theta_numerator / head_dim)).to(device) # (Dim / 2)
+    # Construct the positions (the "m" parameter)
+    # Shape: (Seq_Len)
+    m = torch.arange(seq_len, device=device)
+    # Multiply each theta by each position using the outer product.
+    # Shape: (Seq_Len) outer_product* (Head_Dim / 2) -> (Seq_Len, Head_Dim / 2)
+    freqs = torch.outer(m, theta).float()
+    # We can compute complex numbers in the polar form c = R * exp(m * theta), where R = 1 as follows:
+    # (Seq_Len, Head_Dim / 2) -> (Seq_Len, Head_Dim / 2)
+    freqs_complex = torch.polar(torch.ones_like(freqs), freqs)
+    return freqs_complex
+
+class RMSNorm(nn.Module):
+
+    def __init__(self, dim: int, eps: float = 1e-6):
+        self.eps = eps 
+        # The gamma parameter
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def _norm(self, x: torch.Tensor):
+        # (B, Seq_len, dim) * (B, Seq_len, 1) = (B, Seq_Len, Dim)
+        # rsqrt: 1 / sqrt(x)
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x: torch.Tensor):
+        # (Dim) * (B, Seq_Len, Dim) = (B, Seq_Len, Dim)
+        return self.weight * self._norm(x.float()).type_as(x)
+
+class EncoderBlock(nn.Module):
+
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+
+        self.n_heads = args.n_heads
+        self.dim = args.dim
+        self.head_dim = args.dim // args.n_heads
+        
+        self.attention = SelfAttention(args)
+        self.feed_forward = FeedForward(args)
+
+        # Normalization before the self attention
+        self.attention_norm = RMSNorm(args.dim, eps = args.norm_eps)
+        # Normalization before the feed forward network
+        self.ffn_norm = RMSNorm(args.dim, eps = args.norm_eps)
+    
+    def forward(self, x: torch.Tensor, start_pos: int, freqs_complex: torch.Tensor):
+        # (B, Seq_len, Dim) + (B, Seq_len, Dim) -> (B, Seq_len, Dim)
+        h = x + self.attention.forward(self.attention_norm(x), start_pos, freqs_complex)
+        out = h + self.feed_forward(self.ffn_norm(h))
+        
+        return out
+
+
+
+
 class Transformer(nn.Module):
 
     def __init__(self, args: ModelArgs) -> None:
         super().__init__()
 
-        assert args.vocab_size != -1 "Vocab size must be set"
+        assert args.vocab_size != -1, "Vocab size must be set"
 
         self.args = args
         self.vocab_size = args.vocab_size
@@ -42,6 +106,31 @@ class Transformer(nn.Module):
 
         self.norm = RMSNorm(args.dim, eps = args.norm_eps)
         self.output = nn.Linear(args.dim, self.vocab_size, bias = False)
+        self.freqs_complex = precompute_theta_pos_frequencies(self.args.dim // self.args.n_heads, self.args.max_seq_len * 2,
+                                                              device = self.args.devices)
+        
+
+    def forward(self, tokens: torch.Tensor, start_pos: int):
+        # (B, Seq_len)
+        batch_size, seq_len = tokens.shape
+        assert seq_len == 1, "Only one token at a time can be processed"
+
+        # (B, Seq_len) -> (B, Seq_len, Dim)
+        h = self.tok_embeddings(tokens)
+
+        # Retrieve the pairs (m, theta) corresponding to the positions [start_pos, start_pos + seq_len]
+        freqs_complex = self.freqs_complex[start_pos:start_pos + seq_len]
+
+        # Consecutively apply all the encoder layers
+        for layer in self.layers:
+            h = layer(h, start_pos, freqs_complex)
+        h = self.norm(h)
+        output = self.output(h).float()
+        return output
+
+
+        
+
         
 
 
