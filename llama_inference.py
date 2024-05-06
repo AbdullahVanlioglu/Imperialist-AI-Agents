a923c6dbb8712f109f61b694c2a7ed49e697c250
AbdullahVanlioglu <a.vanli2019@gmail.com>
2024-05-06 22:42:56 +0300

    banKAI

--- llama_inference.py
+++ llama_inference.py
@@ -52,6 +52,61 @@ class LLaMA:
 
         return LLaMA(model, tokenizer, model_args)
 
+    def texxt_completion(self, prompts: List[str], temperature: float = 0.6, top_p: float = 0.9, max_gen_len: Optional[int] = None):
+        if max_gen_len is None:
+            max_gen_len = self.args.max_seq_len - 1
+        # Convert each prompt into tokens
+        prompt_tokens = [self.tokenizer.encode(prompt, out_type=int, add_bos=True, add_eos=False) for prompt in prompts]
+        # Make sure the batch size is not too large
+        batch_size = len(prompt_tokens)
+        assert batch_size <= self.args.max_batch_size
+        max_prompt_len = max(len(prompt) for prompt in prompt_tokens)
+        # Make sure the prompt lenght is not larger than the maximum seq lenght
+        assert max_prompt_len <= self.args.max_seq_len
+        total_len = min(self.args.max_seq_len, max_gen_len + max_prompt_len)
+
+        # Create the list that will contain the generated tokens, along with the initial prompt tokens
+        pad_id = self.tokenizer.pad_id()
+        tokens = torch.full((batch_size, total_len), pad_id, dtype=torch.long, device=device)
+        for k, t in enumerate(prompt_tokens):
+            # Populate the initial tokens with the prompt tokens
+            tokens[k, :len(t)] = torch.tensor(t, dtype=torch.long, device=device)
+
+        eos_reached = torch.Tensor([False] * batch_size, device=device)
+        prompt_tokens_mask = tokens != pad_id # True if the token is a prompt token, False otherwise
+        for cur_pos in tqdm(range(1, total_len), desc='Generating tokens'):
+            with torch.no_grad():
+                logits = self.model.forward(tokens[:, cur_pos-1:cur_pos], cur_pos)
+
+            if temperature > 0:
+                #The temperature is applied BEFORE the softmax
+                probs = torch.softmax(logits[:, -1] / temperature, dim = -1)
+                next_token = self._sample_top_p(probs, top_p)
+            else:
+                # Greedily select the token with the maximum probability
+                next_token = torch.argmax(logits[:, -1 ], dim= -1)
+
+            next_token = next_token.reshape(-1)
+            # Only replace the token if it is a padding token
+            next_token = torch.where(prompt_tokens_mask[:, cur_pos], tokens[:, cur_pos], next_token)
+            tokens[:, cur_pos] = next_token
+            # EOS is reached only if we found an EOS token for a padding position
+            eos_reached |= (~prompt_tokens_mask[:, cur_pos]) & (next_token == self.tokenizer.eos_id)
+            if all(eos_reached):
+                break
+
+            out_tokens = []
+            out_text = []
+            for prompt_index, current_prompt_tokens in enumerate(tokens.tolist()):
+                # Cut to the EOS token, if present
+                if self.tokenizer.eos_id in current_prompt_tokens:
+                    eos_idx = current_prompt_tokens.index(self.tokenizer.eos_id)
+                    current_prompt_tokens = current_prompt_tokens[:eos_idx]
+                out_tokens.append(current_prompt_tokens)
+                out_text.append(self.tokenizer.decode(current_prompt_tokens))
+            return (out_tokens, out_text)
+
+
 
 
 if __name__ == '__main__':
@@ -68,3 +123,5 @@ if __name__ == '__main__':
         max_batch_size=3,
         device=device
     )
+
+    # Inference the model
