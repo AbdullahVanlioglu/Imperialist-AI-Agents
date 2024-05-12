205661ae9b80eec1d02916d267e922403921bc7f
AbdullahVanlioglu <a.vanli2019@gmail.com>
2024-05-12 17:33:25 +0300

    RA SEN GAN

---
+++ decision_transformer_main.py
@@ -0,0 +1,89 @@
+import gym
+import numpy as np
+import torch
+import random
+import argparse
+
+def experiment(
+        exp,
+        variant,
+):
+    device = variant.get('device', 'cuda')
+    log_to_wandb = variant.get('log_to_wandb', False)
+
+    env_name, dataset = variant['env'], variant['dataset']
+    model_type = variant['model_type']
+    group_name = f'{exp}-{env_name}-{dataset}'
+    exp = f'{group_name}-{random.randint(int(1e5), int(1e6) - 1)}'
+
+    if env_name == 'hopper':
+        env = gym.make('Hopper-v3')
+        max_ep_len = 1000
+        env_targets = [3600, 1800]  # evaluation conditioning targets
+        scale = 1000.  # normalization for rewards/returns
+    elif env_name == 'halfcheetah':
+        env = gym.make('HalfCheetah-v3')
+        max_ep_len = 1000
+        env_targets = [12000, 6000]
+        scale = 1000.
+    elif env_name == 'walker2d':
+        env = gym.make('Walker2d-v3')
+        max_ep_len = 1000
+        env_targets = [5000, 2500]
+        scale = 1000.
+    elif env_name == 'reacher2d':
+        from environment.reacher_2d import Reacher2dEnv
+        env = Reacher2dEnv()
+        max_ep_len = 100
+        env_targets = [76, 40]
+        scale = 10.
+    else:
+        raise NotImplementedError
+    
+    if model_type == 'bc':
+        env_targets = env_targets[:1]  # since BC ignores target, no need for different evaluations
+
+    state_dim = env.observation_space.shape[0]
+    act_dim = env.action_space.shape[0]
+
+    # load dataset
+    dataset_path = f'data/{env_name}-{dataset}-v2.pkl'
+    with open(dataset_path, 'rb') as f:
+        trajectories = pickle.load(f)
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env', type=str, default='hopper')
+    parser.add_argument('--dataset', type=str, default='medium')  # medium, medium-replay, medium-expert, expert
+    parser.add_argument('--mode', type=str, default='normal')  # normal for standard setting, delayed for sparse
+    parser.add_argument('--K', type=int, default=20)
+    parser.add_argument('--pct_traj', type=float, default=1.)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--model_type', type=str, default='dt')  # dt for decision transformer, bc for behavior cloning
+    parser.add_argument('--embed_dim', type=int, default=128)
+    parser.add_argument('--n_layer', type=int, default=3)
+    parser.add_argument('--n_head', type=int, default=1)
+    parser.add_argument('--activation_function', type=str, default='relu')
+    parser.add_argument('--dropout', type=float, default=0.1)
+    parser.add_argument('--learning_rate', '-lr', type=float, default=1e-4)
+    parser.add_argument('--weight_decay', '-wd', type=float, default=1e-4)
+    parser.add_argument('--warmup_steps', type=int, default=10000)
+    parser.add_argument('--num_eval_episodes', type=int, default=100)
+    parser.add_argument('--max_iters', type=int, default=10)
+    parser.add_argument('--num_steps_per_iter', type=int, default=10000)
+    parser.add_argument('--device', type=str, default='cuda')
+    parser.add_argument('--log_to_wandb', '-w', type=bool, default=False)
+    
+    args = parser.parse_args()
+
+    experiment('gym-experiment', variant=vars(args))
