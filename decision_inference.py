03ecb931d9b3adff0b3459c4d7d893573ebecfca
AbdullahVanlioglu <a.vanli2019@gmail.com>
2024-05-19 16:51:03 +0300

    Sono Sharingan

--- decision_inference.py
+++ decision_inference.py
@@ -271,45 +271,45 @@ def experiment(exp, config):
             replace=True,
             p=p_sample)  # reweights so we sample according to timesteps
 
-        s, a, r, d, rtg, timesteps, mask = [], [], [], [], [], [], []
+        batch_state, batch_action, batch_reward, batch_done, batch_rtg, batch_timestep, batch_mask = [], [], [], [], [], [], []
         for i in range(batch_size):
             traj = trajectories[int(sorted_inds[batch_inds[i]])]
             si = random.randint(0, traj['rewards'].shape[0] - 1)
 
             # get sequences from dataset
-            s.append(traj['observations'][si:si + max_len].reshape(1, -1, state_dim))
-            a.append(traj['actions'][si:si + max_len].reshape(1, -1, act_dim))
-            r.append(traj['rewards'][si:si + max_len].reshape(1, -1, 1))
+            batch_state.append(traj['observations'][si:si + max_len].reshape(1, -1, state_dim))
+            batch_action.append(traj['actions'][si:si + max_len].reshape(1, -1, act_dim))
+            batch_reward.append(traj['rewards'][si:si + max_len].reshape(1, -1, 1))
             if 'terminals' in traj:
-                d.append(traj['terminals'][si:si + max_len].reshape(1, -1))
+                batch_done.append(traj['terminals'][si:si + max_len].reshape(1, -1))
             else:
-                d.append(traj['dones'][si:si + max_len].reshape(1, -1))
-            timesteps.append(np.arange(si, si + s[-1].shape[1]).reshape(1, -1))
-            timesteps[-1][timesteps[-1] >= max_ep_len] = max_ep_len-1  # padding cutoff
-            rtg.append(discount_cumsum(traj['rewards'][si:], gamma=1.)[:s[-1].shape[1] + 1].reshape(1, -1, 1))
-            if rtg[-1].shape[1] <= s[-1].shape[1]:
-                rtg[-1] = np.concatenate([rtg[-1], np.zeros((1, 1, 1))], axis=1)
+                batch_done.append(traj['dones'][si:si + max_len].reshape(1, -1))
+            batch_timestep.append(np.arange(si, si + batch_state[-1].shape[1]).reshape(1, -1))
+            batch_timestep[-1][batch_timestep[-1] >= max_ep_len] = max_ep_len-1  # padding cutoff
+            batch_rtg.append(discount_cumsum(traj['rewards'][si:], gamma=1.)[:batch_state[-1].shape[1] + 1].reshape(1, -1, 1))
+            if batch_rtg[-1].shape[1] <= batch_state[-1].shape[1]:
+                batch_rtg[-1] = np.concatenate([rtg[-1], np.zeros((1, 1, 1))], axis=1)
     
             # padding and state + reward normalization
-            tlen = s[-1].shape[1]
-            s[-1] = np.concatenate([np.zeros((1, max_len - tlen, state_dim)), s[-1]], axis=1)
-            s[-1] = (s[-1] - state_mean) / state_std
-            a[-1] = np.concatenate([np.ones((1, max_len - tlen, act_dim)) * -10., a[-1]], axis=1)
-            r[-1] = np.concatenate([np.zeros((1, max_len - tlen, 1)), r[-1]], axis=1)
-            d[-1] = np.concatenate([np.ones((1, max_len - tlen)) * 2, d[-1]], axis=1)
-            rtg[-1] = np.concatenate([np.zeros((1, max_len - tlen, 1)), rtg[-1]], axis=1) / scale
-            timesteps[-1] = np.concatenate([np.zeros((1, max_len - tlen)), timesteps[-1]], axis=1)
-            mask.append(np.concatenate([np.zeros((1, max_len - tlen)), np.ones((1, tlen))], axis=1))
+            tlen = batch_state[-1].shape[1]
+            batch_state[-1] = np.concatenate([np.zeros((1, max_len - tlen, state_dim)), batch_state[-1]], axis=1)
+            batch_state[-1] = (batch_state[-1] - state_mean) / state_std
+            batch_action[-1] = np.concatenate([np.ones((1, max_len - tlen, act_dim)) * -10., batch_action[-1]], axis=1)
+            batch_reward[-1] = np.concatenate([np.zeros((1, max_len - tlen, 1)), batch_reward[-1]], axis=1)
+            batch_done[-1] = np.concatenate([np.ones((1, max_len - tlen)) * 2, batch_done[-1]], axis=1)
+            batch_rtg[-1] = np.concatenate([np.zeros((1, max_len - tlen, 1)), batch_rtg[-1]], axis=1) / scale
+            batch_timestep[-1] = np.concatenate([np.zeros((1, max_len - tlen)), batch_timestep[-1]], axis=1)
+            batch_mask.append(np.concatenate([np.zeros((1, max_len - tlen)), np.ones((1, tlen))], axis=1))
 
-        s = torch.from_numpy(np.concatenate(s, axis=0)).to(dtype=torch.float32, device=device)
-        a = torch.from_numpy(np.concatenate(a, axis=0)).to(dtype=torch.float32, device=device)
-        r = torch.from_numpy(np.concatenate(r, axis=0)).to(dtype=torch.float32, device=device)
-        d = torch.from_numpy(np.concatenate(d, axis=0)).to(dtype=torch.long, device=device)
-        rtg = torch.from_numpy(np.concatenate(rtg, axis=0)).to(dtype=torch.float32, device=device)
-        timesteps = torch.from_numpy(np.concatenate(timesteps, axis=0)).to(dtype=torch.long, device=device)
-        mask = torch.from_numpy(np.concatenate(mask, axis=0)).to(device=device)
+        batch_state = torch.from_numpy(np.concatenate(batch_state, axis=0)).to(dtype=torch.float32, device=device)
+        batch_action = torch.from_numpy(np.concatenate(batch_action, axis=0)).to(dtype=torch.float32, device=device)
+        batch_reward = torch.from_numpy(np.concatenate(batch_reward, axis=0)).to(dtype=torch.float32, device=device)
+        batch_done = torch.from_numpy(np.concatenate(batch_done, axis=0)).to(dtype=torch.long, device=device)
+        batch_rtg = torch.from_numpy(np.concatenate(batch_rtg, axis=0)).to(dtype=torch.float32, device=device)
+        batch_timestep = torch.from_numpy(np.concatenate(batch_timestep, axis=0)).to(dtype=torch.long, device=device)
+        batch_mask = torch.from_numpy(np.concatenate(batch_mask, axis=0)).to(device=device)
 
-        return s, a, r, d, rtg, timesteps, mask
+        return batch_state, batch_action, batch_reward, batch_done, batch_rtg, batch_timestep, batch_mask
     
     def eval_episodes(target_rew):
         def fn(model):
