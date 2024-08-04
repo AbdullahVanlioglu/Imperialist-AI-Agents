2fb4ab1733f0a86f9adfd5eedff43d0ba4b9776b
AbdullahVanlioglu <a.vanli2019@gmail.com>
2024-08-04 22:14:05 +0300

    Mangekyo Sharingan

--- rlh_inference.py
+++ rlh_inference.py
@@ -1,9 +1,11 @@
 import torch
 import wandb
+import tqdm
 
 from trl import PPOTrainer, PPOConfig
 from datasets import load_dataset
-from transformers import AutoTokenizer, AutoModel, AutoModelForCausalLMWithValueHead
+from transformers import pipeline, AutoTokenizer, AutoModel, AutoModelForCausalLMWithValueHead
+from trl.core import LengthSampler
 
 
 wandb.init()
@@ -14,25 +16,53 @@ tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-uncased")
 model = AutoModel.from_pretrained("google-bert/bert-base-uncased")
 
 
-config = PPOConfig(
-        model_name="lvwerra/gpt2-imdb",
-        learning_rate=1.41e-5,
-        log_with="wandb")
-
-model = AutoModelForCausalLMWithValueHead.from_pretrained(config.model_name)
-ref_model = AutoModelForCausalLMWithValueHead.from_pretrained(config.model_name)
+def collator(data):
+    return dict((key, [d[key] for d in data]) for key in data[0])
 
 
-tokenizer = AutoTokenizer.from_pretrained(config.model_name)
-tokenizer.pad_token = tokenizer.eos_token
+if __name__ == '__main__':
+        config = PPOConfig(
+                model_name="lvwerra/gpt2-imdb",
+                learning_rate=1.41e-5,
+                log_with="wandb")
 
-ppo_trainer = PPOTrainer(config, model, ref_model, tokenizer, dataset=dataset, data_collator=collator)
+        model = AutoModelForCausalLMWithValueHead.from_pretrained(config.model_name)
+        ref_model = AutoModelForCausalLMWithValueHead.from_pretrained(config.model_name)
 
-sentiment_pipe = pipeline("sentiment-analysis", model="Lvwerra/distilbert-imdb", device=device)
 
-sent_kwargs = {"return_all_scores": True, "function_to_apply": "none", "batch_size": 16}
-text = "this movie was really bad!!"
-print(sentiment_pipe(text, **sent_kwargs))
+        tokenizer = AutoTokenizer.from_pretrained(config.model_name)
+        tokenizer.pad_token = tokenizer.eos_token
 
-text = "this movie was really good!!"
-print(sentiment_pipe(text, **sent_kwargs)) # [{'label': 'NEGATIVE', 'score': -2.335047960281372}, {'label': 'POSITIVE', 'score': 2.557039737701416}]
+        ppo_trainer = PPOTrainer(config, model, ref_model, tokenizer, dataset=dataset, data_collator=collator)
+
+        sentiment_pipe = pipeline("sentiment-analysis", model="lvwerra/distilbert-imdb", device=device)
+
+        sent_kwargs = {"return_all_scores": True, "function_to_apply": "none", "batch_size": 16}
+        text = "this movie was really bad!!"
+        print(sentiment_pipe(text, **sent_kwargs))
+
+        text = "this movie was really good!!"
+        print(sentiment_pipe(text, **sent_kwargs)) # [{'label': 'NEGATIVE', 'score': -2.335047960281372}, {'label': 'POSITIVE', 'score': 2.557039737701416}]
+        
+        output_min_length = 4
+        output_max_length = 16
+        output_length_sampler = LengthSampler(output_min_length, output_max_length)
+
+        response_generation_kwargs = {
+                "min_length": -1,
+                "top_k": 0.0,
+                "top_p": 1.0,
+                "do_sample": True,
+                "pad_token_id": tokenizer.eos_token_id
+                }
+        
+        for epoch, batch in tqdm(enumerate(ppo_trainer.dataloader)):
+                query_tensors = batch["input_ids"]
+
+                response_tensors = []
+                for query in query_tensors:
+                       gen_len = output_length_sampler()
+                       response_generation_kwargs["max_new_tokens"] = gen_len
+                       response = ppo_trainer.generate(query, **response_generation_kwargs)
+                       response_tensors.append(response.squeeze()[-gen_len:])
+                batch["response"] = [tokenizer.decode(r.squeeze()) for r in response_tensors] 
