672aa71b20768d7a5d9caf55c1ed93084552d558
AbdullahVanlioglu <a.vanli2019@gmail.com>
2024-08-04 22:33:51 +0300

    Mangekyo Sharingan

--- rlh_inference.py
+++ rlh_inference.py
@@ -21,48 +21,55 @@ def collator(data):
 
 
 if __name__ == '__main__':
-        config = PPOConfig(
-                model_name="lvwerra/gpt2-imdb",
-                learning_rate=1.41e-5,
-                log_with="wandb")
+	config = PPOConfig(
+		model_name="lvwerra/gpt2-imdb",
+		learning_rate=1.41e-5,
+		log_with="wandb")
 
-        model = AutoModelForCausalLMWithValueHead.from_pretrained(config.model_name)
-        ref_model = AutoModelForCausalLMWithValueHead.from_pretrained(config.model_name)
+	model = AutoModelForCausalLMWithValueHead.from_pretrained(config.model_name)
+	ref_model = AutoModelForCausalLMWithValueHead.from_pretrained(config.model_name)
 
 
-        tokenizer = AutoTokenizer.from_pretrained(config.model_name)
-        tokenizer.pad_token = tokenizer.eos_token
+	tokenizer = AutoTokenizer.from_pretrained(config.model_name)
+	tokenizer.pad_token = tokenizer.eos_token
 
-        ppo_trainer = PPOTrainer(config, model, ref_model, tokenizer, dataset=dataset, data_collator=collator)
+	ppo_trainer = PPOTrainer(config, model, ref_model, tokenizer, dataset=dataset, data_collator=collator)
 
-        sentiment_pipe = pipeline("sentiment-analysis", model="lvwerra/distilbert-imdb", device=device)
+	sentiment_pipe = pipeline("sentiment-analysis", model="lvwerra/distilbert-imdb", device=device)
 
-        sent_kwargs = {"return_all_scores": True, "function_to_apply": "none", "batch_size": 16}
-        text = "this movie was really bad!!"
-        print(sentiment_pipe(text, **sent_kwargs))
+	sent_kwargs = {"return_all_scores": True, "function_to_apply": "none", "batch_size": 16}
+	text = "this movie was really bad!!"
+	print(sentiment_pipe(text, **sent_kwargs))
 
-        text = "this movie was really good!!"
-        print(sentiment_pipe(text, **sent_kwargs)) # [{'label': 'NEGATIVE', 'score': -2.335047960281372}, {'label': 'POSITIVE', 'score': 2.557039737701416}]
-        
-        output_min_length = 4
-        output_max_length = 16
-        output_length_sampler = LengthSampler(output_min_length, output_max_length)
+	text = "this movie was really good!!"
+	print(sentiment_pipe(text, **sent_kwargs)) # [{'label': 'NEGATIVE', 'score': -2.335047960281372}, {'label': 'POSITIVE', 'score': 2.557039737701416}]
+	
+	output_min_length = 4
+	output_max_length = 16
+	output_length_sampler = LengthSampler(output_min_length, output_max_length)
 
-        response_generation_kwargs = {
-                "min_length": -1,
-                "top_k": 0.0,
-                "top_p": 1.0,
-                "do_sample": True,
-                "pad_token_id": tokenizer.eos_token_id
-                }
-        
-        for epoch, batch in tqdm(enumerate(ppo_trainer.dataloader)):
-                query_tensors = batch["input_ids"]
+	response_generation_kwargs = {
+		"min_length": -1,
+		"top_k": 0.0,
+		"top_p": 1.0,
+		"do_sample": True,
+		"pad_token_id": tokenizer.eos_token_id
+		}
+	
+	for epoch, batch in tqdm(enumerate(ppo_trainer.dataloader)):
+		query_tensors = batch["input_ids"]
 
-                response_tensors = []
-                for query in query_tensors:
-                       gen_len = output_length_sampler()
-                       response_generation_kwargs["max_new_tokens"] = gen_len
-                       response = ppo_trainer.generate(query, **response_generation_kwargs)
-                       response_tensors.append(response.squeeze()[-gen_len:])
-                batch["response"] = [tokenizer.decode(r.squeeze()) for r in response_tensors] 
+		response_tensors = []
+		for query in query_tensors:
+			gen_len = output_length_sampler()
+			response_generation_kwargs["max_new_tokens"] = gen_len
+			response = ppo_trainer.generate(query, **response_generation_kwargs)
+			response_tensors.append(response.squeeze()[-gen_len:])
+		batch["response"] = [tokenizer.decode(r.squeeze()) for r in response_tensors]
+
+		texts = [q + r for q,r in zip(batch["query"], batch["response"])]
+		pipe_outputs = sentiment_pipe(texts, **sent_kwargs)
+
+		rewards = [torch.tensor(output[1]["score"]) for output in pipe_outputs]
+
+		stats = ppo_trainer.step(query_tensors, response_tensors, rewards)
