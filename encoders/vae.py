366f0b5cdbc1cb83e547ade1fe73109a823d3b28
AbdullahVanlioglu <a.vanli2019@gmail.com>
2024-08-15 21:29:52 +0300

    BAN KAI

--- encoders/vae.py
+++ encoders/vae.py
@@ -1,3 +1,15 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
+
+class VAE_Encoder(nn.Sequential):
+    def __init__(self):
+        super().__init__(
+            # (Batch_Size, Channel, Height, Width) -> (Batch_Size, 128, Height, Width)
+            nn.Conv2d(3, 128, kernel_size=3, padding=1),
+
+            # (Batch_Size, 128, Height, Width) -> (Batch_Size, 128, Height, Width)
+            VAE_ResidualBlock(128, 128),
+            
+
+        )
