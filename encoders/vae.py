809e2eb24a98aa93dcae7cc00d4269ba5a197112
AbdullahVanlioglu <a.vanli2019@gmail.com>
2024-08-18 00:18:34 +0300

    Mangekyo Sharingan

--- encoders/vae.py
+++ encoders/vae.py
@@ -2,6 +2,39 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
+
+class VAE_ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.groupnorm_1 = nn.GroupNorm(32, in_channels)
+        self.conv_1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+
+        self.groupnorm_2 = nn.GroupNorm(32, out_channels)
+        self.conv_2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+
+        if in_channels == out_channels:
+            self.residual_layer = nn.Identity()
+        else:
+            self.residual_layer = nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0)
+
+    def forwrad(self, x: torch.Tensor) -> torch.Tensor:
+        # x: (Batch_Size, In_channels, Height, Width)
+        residue = x
+
+        x = self.groupnorm_1(x)
+        x = F.silu(x)
+        x = self.conv_1(x)
+
+        x = self.groupnorm_2(x)
+        x = F.silu(x)
+        x = self.conv_2(x)
+
+        return x + self.residual_layer(residue)
+
+
+
+
+
 class VAE_Encoder(nn.Sequential):
     def __init__(self):
         super().__init__(
@@ -89,3 +122,4 @@ class VAE_Encoder(nn.Sequential):
         output *= 0.18215
 
         return output
+    
