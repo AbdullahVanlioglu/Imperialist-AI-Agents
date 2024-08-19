6147a5ef788959e47407a67dbc32fde0b1eab4af
AbdullahVanlioglu <a.vanli2019@gmail.com>
2024-08-19 20:18:18 +0300

    Koto Amatsukami

--- transformer/diffusion.py
+++ transformer/diffusion.py
@@ -3,6 +3,22 @@ from torch.nn import nn
 from torch.nn.functional import functional as F
 from attentions.attention import SelfAttention, CrossAttention
 
+class TimeEmbedding(nn.Module):
+    def __init__(self, n_embd: int):
+        super().__init__()
+        self.linear_1 = nn.Linear(n_embd, 4 * n_embd)
+        self.linear_2 = nn.Linear(4 * n_embd, 4 * n_embd)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: (1, 320)
+
+        x = self.linear_1(x)
+        x = F.silu(x)
+        x = self.linear_2(x)
+        # (1, 1280)
+        return x 
+
+
 class Diffusion(nn.Module):
     def __init__(self):
         self.time_embedding = TimeEmbedding(320)
@@ -14,3 +30,15 @@ class Diffusion(nn.Module):
         # context: (Batch_Size, Seq_Len, Dim)
         # time: (1, 320)
         
+        # (1, 320) -> (1, 1280)
+        time = self.time_embedding(time)
+
+        # (Batch_Size, 4, Height/8, Width/8) -> (Batch_Size, 320, Height/8, Width/8)
+        output = self.unet(latent, context, time)
+
+        # (Batch_Size, 320, Height/8, Width/8) -> (Batch_Size, 4, Height/8, Width/8)
+        output = self.final(output)
+
+        return output
+
+
