926345610e2339c8dce8b29a4148cd3b9f973e81
Abdullah Vanlioglu <a.vanli2019@gmail.com>
2024-09-10 22:05:39 +0300

    Kage Bunshin No Jutsu

--- transformer/diffusion_transformer.py
+++ transformer/diffusion_transformer.py
@@ -1,3 +1,25 @@
 import torch
 import math
 import torch.nn as nn
+
+class PatchEmbed(nn.Module):
+    def __init__(self,
+                 height:int,
+                 width:int,
+                 patch_size:int,
+                 layer_norm:bool=False,
+                 flatten_output:bool=True,
+                 ):
+        super().__init__()
+
+        self.flatten = flatten_output
+        self.layer_norm = layer_norm
+
+        num_patches = (height // patch_size) * (width // patch_size)
+        
+
+
+class DiT(nn.Module):
+    def __init__(self):
+        
+        self.patch_embed = self.PatchEmbed()
